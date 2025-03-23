f806c3b162b8a887d5ac11b2e943230b6e13f8ef
MD Fahim <fmn.flame@gmail.com>
2025-03-23 22:21:25 +0600

    good night

--- lib/UI/Screens/add_new_task_screen.dart
+++ lib/UI/Screens/add_new_task_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager/UI/Widgets/task_card.dart';
+import 'package:task_manager/UI/Widgets/screen_background.dart';
+import 'package:task_manager/UI/Widgets/t_m_app_bar.dart';
 
 class AddNewTaskScreen extends StatefulWidget {
   const AddNewTaskScreen({super.key});
@@ -11,25 +12,50 @@ class AddNewTaskScreen extends StatefulWidget {
 class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.separated(
-              itemCount: 10,
-              primary: false,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                
-                return TaskCard(
-                  taskStatus: TaskStatus.cancelled,
-                );
-              },
-              separatorBuilder: (context, index) => SizedBox(
-                height: 8,
-              ),
-            )
-          ],
+      appBar: TMAppBar(textTheme: textTheme),
+      body: ScreenBackground(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  "Add new Task",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    hintText: "Subject",
+                  ),
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                TextFormField(
+                  maxLines: 8,
+                  decoration: InputDecoration(
+                    hintText: "Description",
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  ),
+                ),
+
+                SizedBox(height: 16,),
+
+                ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_circle_right_outlined,color: Colors.white,size: 24,))
+              ],
+            ),
+          ),
         ),
       ),
     );
